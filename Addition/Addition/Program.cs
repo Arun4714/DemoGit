8e2fa9b3283c986324184174bc456ac96af8d7bf
Arun <SOE-Load@IN5CG8405701>
2019-06-20 15:33:41 +0530

    Program written

---
+++ Addition/Addition/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            Console.WriteLine("Sum = {0}", a + b);
+            Console.ReadKey();
+        }
+    }
+}
